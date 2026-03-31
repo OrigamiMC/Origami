75811597744c346ff84f7b3aaed74ab4a60a7cc0
Oliver <oschi1303@gmail.com>
2026-03-31 18:44:27 +0200

    Catch unexpected expections and improve logging

--- origami-server/src/main/java/com/origamimc/origami/OrigamiServerImpl.java
+++ origami-server/src/main/java/com/origamimc/origami/OrigamiServerImpl.java
@@ -2,6 +2,7 @@ package com.origamimc.origami;
 
 import com.origamimc.origami.api.Origami;
 import com.origamimc.origami.plugins.PluginServiceImpl;
+import de.oliver.fancyanalytics.logger.properties.ThrowableProperty;
 
 public class OrigamiServerImpl {
 
@@ -9,48 +10,71 @@ public class OrigamiServerImpl {
      * Called right when the server boots up
      */
     public void onBoot() {
-        Origami.logger().info("Booting Origami ...");
-
-        Origami.logger().info("Successfully booted Origami");
+        try {
+            PluginServiceImpl.get().loadFromPluginsFolder();
+            Origami.logger().info("Scanned all plugins from plugins folder");
+        } catch (Exception e) {
+            Origami.logger().error("Unexpected exception in onBoot", new ThrowableProperty(e));
+        }
     }
 
     /**
      * Called before worlds are being loaded
      */
     public void onPrepareLevels() {
-        PluginServiceImpl.get().loadFromPluginsFolder();
+        try {
+
+        } catch (Exception e) {
+            Origami.logger().error("Unexpected exception in onPrepareLevels", new ThrowableProperty(e));
+        }
     }
 
     /**
      * Called when the server is starting
      */
     public void onServerStarting() {
-
+        try {
+            PluginServiceImpl.get().loadPlugins();
+            Origami.logger().info("Loaded all origami plugins");
+        } catch (Exception e) {
+            Origami.logger().error("Unexpected exception in onServerStarting", new ThrowableProperty(e));
+        }
     }
 
     /**
      * Called when the server is started
      */
     public void onServerStarted() {
-        Origami.logger().info("Starting Origami ...");
+        try {
+            PluginServiceImpl.get().enablePlugins();
+            Origami.logger().info("Enabled all origami plugins");
+        } catch (Exception e) {
+            Origami.logger().error("Unexpected exception in onServerStarted", new ThrowableProperty(e));
+        }
 
-        PluginServiceImpl.get().enablePlugins();
-
-        Origami.logger().info("Successfully started Origami");
     }
 
     /**
      * Called when the server is stopping
      */
     public void onServerStopping() {
-
+        try {
+            PluginServiceImpl.get().disablePlugins();
+            Origami.logger().info("Disabled all origami plugins");
+        } catch (Exception e) {
+            Origami.logger().error("Unexpected exception in onServerStopping", new ThrowableProperty(e));
+        }
     }
 
     /**
      * Called when the server is stopped
      */
     public void onServerStopped() {
+        try {
 
+        } catch (Exception e) {
+            Origami.logger().error("Unexpected exception in onServerStopped", new ThrowableProperty(e));
+        }
     }
 
 }
--- origami-server/src/main/java/com/origamimc/origami/plugins/PluginServiceImpl.java
+++ origami-server/src/main/java/com/origamimc/origami/plugins/PluginServiceImpl.java
@@ -5,6 +5,7 @@ import com.origamimc.origami.api.plugins.OrigamiPlugin;
 import com.origamimc.origami.api.plugins.PluginManifest;
 import com.origamimc.origami.api.plugins.PluginService;
 import com.origamimc.origami.plugins.builtin.timer.TimerPlugin;
+import de.oliver.fancyanalytics.logger.properties.StringProperty;
 
 import java.util.List;
 import java.util.Map;
@@ -26,21 +27,59 @@ public class PluginServiceImpl implements PluginService {
     public void loadFromPluginsFolder() {
         TimerPlugin timerPlugin = new TimerPlugin(new PluginManifest("timer", "1.0.0", "com.origamimc.origami.plugins.builtin.timer.TimerPlugin"));
         plugins.put(timerPlugin.getManifest().name(), timerPlugin);
+    }
 
+    public void loadPlugins() {
         for (OrigamiPlugin plugin : plugins.values()) {
+            Origami.logger().info(
+                    "Loading the " + plugin.getManifest().name() + " plugin",
+                    StringProperty.of("plugin", plugin.getManifest().name()),
+                    StringProperty.of("version", plugin.getManifest().version())
+            );
+
             plugin.load();
+
+            Origami.logger().info(
+                    "Loaded the " + plugin.getManifest().name() + " plugin",
+                    StringProperty.of("plugin", plugin.getManifest().name()),
+                    StringProperty.of("version", plugin.getManifest().version())
+            );
         }
     }
 
     public void enablePlugins() {
         for (OrigamiPlugin plugin : plugins.values()) {
+            Origami.logger().info(
+                    "Enabling the " + plugin.getManifest().name() + " plugin",
+                    StringProperty.of("plugin", plugin.getManifest().name()),
+                    StringProperty.of("version", plugin.getManifest().version())
+            );
+
             plugin.enable();
+
+            Origami.logger().info(
+                    "Enabled the " + plugin.getManifest().name() + " plugin",
+                    StringProperty.of("plugin", plugin.getManifest().name()),
+                    StringProperty.of("version", plugin.getManifest().version())
+            );
         }
     }
 
     public void disablePlugins() {
         for (OrigamiPlugin plugin : plugins.values()) {
+            Origami.logger().info(
+                    "Disabling the " + plugin.getManifest().name() + " plugin",
+                    StringProperty.of("plugin", plugin.getManifest().name()),
+                    StringProperty.of("version", plugin.getManifest().version())
+            );
+
             plugin.disable();
+
+            Origami.logger().info(
+                    "Disabled the " + plugin.getManifest().name() + " plugin",
+                    StringProperty.of("plugin", plugin.getManifest().name()),
+                    StringProperty.of("version", plugin.getManifest().version())
+            );
         }
     }
 
